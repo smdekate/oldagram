3d224445246f5b72f797dc6e6f87274e6421f9d0
smdekate <smdekate@gmail.com>
2023-07-26 14:41:30 +0530

    done with the app
    only final touches are left

---
+++ data.js
@@ -0,0 +1,31 @@
+export const userData = [
+    {
+        "profile-pic": "images/avatar-vangogh.jpg",
+        "name": "Vincent Van Gogh",
+        "location": "Zudert, Netherlands",
+        "post": "/images/post-vangogh.jpg",
+        "likes": 21492,
+        "comment": "Just took a few mushroom lol",
+        "friend": "vincey1853"
+    },
+
+    {
+        "profile-pic": "images/avatar-courbet.jpg",
+        "name": "Gustav Courbet",
+        "location": "Ornas, France",
+        "post": "images/post-courbet.jpg",
+        "likes": 12502,
+        "comment": "i'm feeleing a bit stressed tbh",
+        "friend": "gus1819"
+    },
+
+    {
+        "profile-pic": "images/avatar-ducreux.jpg",
+        "name": "Joseph Ducreux",
+        "location": "Paris, France",
+        "post": "images/post-ducreux.jpg",
+        "likes": 15137,
+        "comment": "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        "friend": "jd1735"
+    },
+]
---
+++ index.js
@@ -0,0 +1,73 @@
+import { userData } from "./data.js"
+
+const main = document.getElementById("main")
+
+let dom = ""
+
+for (let user of userData) {
+    dom += `
+        <section class="card container">
+            <div class="user-info">
+                <img class="user-img navbar-img" src="${user["profile-pic"]}" alt="${user.name} + ''s profile picture'">
+                <div>
+                    <p class="user-name">${user.name}</p>
+                    <p class="location">${user.location}</p>
+                </div>
+            </div>
+
+            <div>
+                <img class="container" src="${user.post}" alt="${user.name} + ''s post'">
+            </div>
+
+            <div>
+                <div class="like-comment-share">
+                    <img class="icon" src="images/icon-heart.png" alt="like button">
+                    <img class="icon" src="images/icon-comment.png" alt="comment button">
+                    <img class="icon" src="images/icon-dm.png" alt="share button">
+                </div>
+                <div>
+                    <p class="likes">${user.likes} likes</p>
+                </div>
+                <div class="comment">
+                    <p class="likes">${user.friend}</p>
+                    <p class="likes comment-msg">${userData[0].comment}</p>
+                </div>
+            </div>
+        </section>
+    `
+}
+
+ main.innerHTML = dom
+
+
+//  PROTOTYPE
+// const post = `
+// //     <section class="card container">
+// //         <div class="user-info">
+// //             <img class="user-img navbar-img" src="${userData[0]["profile-pic"]}" alt="${userData[0].name} + ''s profile picture'">
+// //             <div>
+// //                 <p class="user-name">${userData[0].name}</p>
+// //                 <p class="location">${userData[0].location}</p>
+// //             </div>
+// //         </div>
+
+// //         <div>
+// //             <img class="container" src="${userData[0].post}" alt="${userData[0].name} + ''s post'">
+// //         </div>
+
+// //         <div>
+// //             <div class="like-comment-share">
+// //                 <img class="icon" src="images/icon-heart.png" alt="like button">
+// //                 <img class="icon" src="images/icon-comment.png" alt="comment button">
+// //                 <img class="icon" src="images/icon-dm.png" alt="share button">
+// //             </div>
+// //             <div>
+// //                 <p class="likes">${userData[0].likes} likes</p>
+// //             </div>
+// //             <div class="comment">
+// //                 <p class="likes">${userData[0].friend}</p>
+// //                 <p class="likes comment-msg">${userData[0].comment}</p>
+// //             </div>
+// //         </div>
+// //     </section>
+// // `
